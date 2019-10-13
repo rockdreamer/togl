07ae69ad0599e6afe811648096b19c73084c80d4
Claudio Bantaloukas <cbantaloukas@ccdc.cam.ac.uk>
2019-10-13 16:57:17 +0100

    use tcl and tk in python27

--- .ci/build.cmd
+++ .ci/build.cmd
@@ -1,18 +1,10 @@
+"C:\Users\appveyor\AppData\Local\Programs\Common\Microsoft\Visual C++ for Python\9.0\vcvarsall.bat" %VCVARSALL_PLATFORM%
 mkdir dist
 mkdir build
 cd build
 cmake --version
-if %VCVARSALL_PLATFORM% == amd64 (
-    set COMPILER_PATH=C:/Users/appveyor/AppData/Local/Programs/Common/Microsoft/Visual C++ for Python/9.0/VC/bin/%VCVARSALL_PLATFORM%
-) else (
-    set COMPILER_PATH=C:/Users/appveyor/AppData/Local/Programs/Common/Microsoft/Visual C++ for Python/9.0/VC/bin
-)
-set PATH=%COMPILER_PATH%;%PATH%
 cmake .. ^
     -DCMAKE_BUILD_TYPE=Release ^
-    "-DCMAKE_C_COMPILER=%COMPILER_PATH%/cl.exe" ^
-    "-DCMAKE_C_COMPILER=%COMPILER_PATH%/cl.exe" ^
-    "-DCMAKE_LINKER=%COMPILER_PATH%/link.exe" ^
     -DCMAKE_INSTALL_PREFIX=..\dist ^
     -G Ninja
 cmake --build .
