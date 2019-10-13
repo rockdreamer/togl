b0047a72274efe170fc3f365224a75f5fc3fc2cc
Claudio Bantaloukas <rockdreamer@gmail.com>
2019-10-13 15:36:11 +0100

    set 32bit explicitly in setenv.cmd

--- .ci/run_with_env.cmd
+++ .ci/run_with_env.cmd
@@ -55,7 +55,7 @@ IF  %VCVARSALL_PLATFORM% == amd64  (
 ) ELSE (
     ECHO Configuring Windows SDK %WINDOWS_SDK_VERSION% for Python %MAJOR_PYTHON_VERSION% on a 32 bit architecture
     "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Setup\WindowsSdkVer.exe" -q -version:%WINDOWS_SDK_VERSION%
-    "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd" /release
+    "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd" /x86 /release
     ECHO Executing: %COMMAND_TO_RUN%
     call %COMMAND_TO_RUN% || EXIT 1
 )
