46efea7db0d83c61abe9f871318497e433cd0029
Claudio Bantaloukas <rockdreamer@gmail.com>
2019-10-13 16:58:55 +0100

    call

--- .ci/build.cmd
+++ .ci/build.cmd
@@ -1,4 +1,4 @@
-"C:\Users\appveyor\AppData\Local\Programs\Common\Microsoft\Visual C++ for Python\9.0\vcvarsall.bat" %VCVARSALL_PLATFORM%
+call "C:\Users\appveyor\AppData\Local\Programs\Common\Microsoft\Visual C++ for Python\9.0\vcvarsall.bat" %VCVARSALL_PLATFORM%
 mkdir dist
 mkdir build
 cd build
