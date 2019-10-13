d4721a3d5bd3838d085869f266ec0e533ccea678
Claudio Bantaloukas <rockdreamer@gmail.com>
2019-10-13 15:15:17 +0100

    fix 32 bit compilers

--- .ci/build.cmd
+++ .ci/build.cmd
@@ -2,11 +2,17 @@ mkdir dist
 mkdir build
 cd build
 cmake --version
+if %VCVARSALL_PLATFORM% == amd64 (
+    set COMPILER_PATH=C:/Users/appveyor/AppData/Local/Programs/Common/Microsoft/Visual C++ for Python/9.0/VC/bin/%VCVARSALL_PLATFORM%
+) else (
+    set COMPILER_PATH=C:/Users/appveyor/AppData/Local/Programs/Common/Microsoft/Visual C++ for Python/9.0/VC/bin
+)
+set PATH=%COMPILER_PATH%;%PATH%
 cmake .. ^
     -DCMAKE_BUILD_TYPE=Release ^
-    "-DCMAKE_C_COMPILER=C:/Users/appveyor/AppData/Local/Programs/Common/Microsoft/Visual C++ for Python/9.0/VC/bin/%VCVARSALL_PLATFORM%/cl.exe" ^
-    "-DCMAKE_C_COMPILER=C:/Users/appveyor/AppData/Local/Programs/Common/Microsoft/Visual C++ for Python/9.0/VC/bin/%VCVARSALL_PLATFORM%/cl.exe" ^
-    "-DCMAKE_LINKER=C:/Users/appveyor/AppData/Local/Programs/Common/Microsoft/Visual C++ for Python/9.0/VC/bin/%VCVARSALL_PLATFORM%/link.exe" ^
+    "-DCMAKE_C_COMPILER=%COMPILER_PATH%/cl.exe" ^
+    "-DCMAKE_C_COMPILER=%COMPILER_PATH%/cl.exe" ^
+    "-DCMAKE_LINKER=%COMPILER_PATH%/link.exe" ^
     -DCMAKE_INSTALL_PREFIX=..\dist ^
     -G Ninja
 cmake --build .
